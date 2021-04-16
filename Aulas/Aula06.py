a7fa7e6fa8504228ebac26f905a2ccbc5b3b2a92
Matheus Higino <matheushigino.nep@gmail.com>
2021-04-16 09:21:56 -0300

    Update Aula06.py

--- Aulas/Aula06.py
+++ Aulas/Aula06.py
@@ -2,3 +2,10 @@ n1 = int(input('Type the first number: '))
 n2 = int(input('Type the second number: '))
 s = n1 + n2
 print('The sum of {} and {} results in {}.'.format(n1, n2, s))
+
+n3 = bool(input('Type a number: '))
+# If the user don't type anything, the result will be 'False'.
+print(n3)
+
+n4 = input('Type any character: ')
+print(n4.isnumeric())
