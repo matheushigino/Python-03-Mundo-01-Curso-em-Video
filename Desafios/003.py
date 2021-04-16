682769edc37089b9cd365944d8972796c65fd783
Matheus Higino <matheushigino.nep@gmail.com>
2021-04-16 09:27:11 -0300

    Create 003.py

---
+++ Desafios/003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+print("A soma entre eles é {}." .format(soma))
